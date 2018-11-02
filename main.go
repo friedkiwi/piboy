94bb2fa61a6055ef170a88523526dcd469e917f1
Yvan Janssens <ik@yvanj.me>
2018-11-02 20:13:14 +0000

    Stubbed linkcable module and added reference to main.go

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/friedkiwi/piboy/linkcable"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	linkcable.Open()
+
+	defer linkcable.Close()
+
+	fmt.Println(linkcable.GetVersion())
 }
